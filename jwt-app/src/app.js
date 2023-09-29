91a12be86e43a56766b82532d60a7b37192c8470
Pedro Leonardo <pedroleon.pc@hotmail.com>
2023-09-29 20:07:06 -0300

    Modified Permission in Cors

--- jwt-app/src/app.js
+++ jwt-app/src/app.js
@@ -7,20 +7,17 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: "*"
+    origin: ["https://auth-project-pi.vercel.app/login", "https://auth-project-pi.vercel.app/register", "https://auth-project-pi.vercel.app/dashboard"]
 }))
+
 app.use(express.json())
 app.use(router)
 
 const db = process.env.DB_ACESS
 
 mongoose.connect(db)
-.then(() => {
-    app.listen({
-        host: '0.0.0.0',
-        port: process.env.PORT ?? 3333
-        })
-})
-.catch((error) => {
-    console.log(error)
+
+app.listen({
+    host: '0.0.0.0',
+    port: process.env.PORT ?? 3333
 })
